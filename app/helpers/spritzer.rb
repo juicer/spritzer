392cac0aa5842720c178a52b60debe94fda63b03
Andrew Butcher <abutcher@redhat.com>
2013-04-27 23:32:38 -0400

    Store this in git. What the hell.

--- app/helpers/spritzer.rb
+++ app/helpers/spritzer.rb
@@ -2,11 +2,9 @@ module Spritzer
   # Allows accessing config variables from spritzer.yml like so:
   # Spritzer[:qa][:password] => Swe3tp@55weRd
 
-  RAILS_ROOT = "/home/abutcher/rhat/spritzer"
-
   def self.[](key)
     unless @config
-      raw_config = File.read(RAILS_ROOT + "/config/spritzer.yml")
+      raw_config = File.read("config/spritzer.yml")
       @config = HashWithIndifferentAccess.new(YAML.load(raw_config))
     end
     @config[key]
