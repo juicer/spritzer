23799be65a577133774da7953af8c642b6a8d8db
Andrew Butcher <abutcher@redhat.com>
2013-10-29 22:18:33 -0400

    Filterable repo list SON.

---
+++ app/assets/javascripts/spritzer.js
@@ -0,0 +1,29 @@
+function repo_list($filter){
+    $('#list-repos li a').each(function(index, repo){
+	var re = new RegExp(".*" + $filter + ".*","i");
+	if (!re.test($(repo).text())){
+	    $(repo).hide();
+	} else {
+	    $(repo).show();
+	}
+    })
+}
+
+$(document).ready(function(){
+    //////////////////////////////////////////////////////////////////
+    // LOAD CARTS BY DEFAULT
+    repo_list('.*');
+
+    //////////////////////////////////////////////////////////////////
+    // INPUT EVENT HANDLERS
+    $("#input-repo-list").on("input", null, null, repo_list_filter);
+});
+
+
+//////////////////////////////////////////////////////////////////
+// CALLBACKS
+
+function repo_list_filter() {
+    var $list_filter = $('#input-repo-list').val();
+    repo_list($list_filter);
+}
